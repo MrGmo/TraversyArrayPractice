4fdca7c397e38ab547c448a35d7422bbd5a1ab5d
Guillermo Aguilar <gaguilar128@gmail.com>
2021-05-13 09:47:14 -0500

    Algo Practice

--- ChallengesOne/challenge1.js
+++ ChallengesOne/challenge1.js
@@ -115,5 +115,34 @@
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
 
+// Solution 1
+// function fizzBuzz(){
+//   for(let i = 1; i <= 100; i++){
+//     if(i % 15 === 0){
+//       console.log('FizzBuzz')
+//     }else if(i % 5 === 0){
+//       console.log('Buzz')
+//     }else if(i % 3 === 0){
+//       console.log('Fizz')
+//     }else{
+//       console.log(i)
+//     }
+//   }
+// }
 
+// fizzBuzz();
 
+// let i = 1
+
+// while(i < 101){
+//   if(i % 15 === 0){
+//     console.log('FizzBuzz')
+//   }else if(i % 5 === 0){
+//     console.log('Buzz')
+//   }else if(i % 3 === 0){
+//     console.log('Fizz')
+//   }else{
+//     console.log(i)
+//   }
+//   i++
+// }
