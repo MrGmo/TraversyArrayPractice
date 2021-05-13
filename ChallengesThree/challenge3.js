678d4292760350a8fac48a0d6a911e3fb8fb3c60
Guillermo Aguilar <gaguilar128@gmail.com>
2021-05-13 09:27:46 -0500

    Algo Practice

--- ChallengesThree/challenge3.js
+++ ChallengesThree/challenge3.js
@@ -2,11 +2,7 @@
 // Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
 // ex. addAll(2,5,6,7) === 20
 
-// Solution 1
-// function addAll(...numbers){
-//   return numbers.reduce((a, b) => a + b, 0)
-// }
-//
+
 // console.log(addAll(2, 5, 6, 7))
 
 
@@ -14,46 +10,14 @@
 // Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
 // ex. sumAllPrimes(10) == 17
 
-// function sumAllPrimes(num){
-//   let total = 0
-//
-//   function primeCheck(i){
-//     for(let j = 2; j < i; j++){
-//       if(i % j === 0){
-//         return false
-//       }
-//     }
-//         return true
-//   }
-//
-//   for(let i = 2; i <= num; i++){
-//     if(primeCheck(i)){
-//       total+=i
-//     }
-//   }
-//   return total
-// }
-//
+
 // console.log(sumAllPrimes(100))
 
 // CHALLENGE 3: SEEK & DESTROY
 // Remove from the array whatever is in the following arguments. Return the leftover values in an array
 // ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
 
-// Solution 1
-// function seekAndDestroy(arr){
-//   const args = Array.from(arguments)
-//   function filterArr(arr){
-//     return args.indexOf(arr) === -1
-//   }
-//   return arr.filter(filterArr)
-// }
 
-// Solution 2
-// function seekAndDestroy(arr, ...rest){
-//   return arr.filter(x => !rest.includes(x))
-// }
-//
 // console.log(seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6))
 
 
@@ -63,22 +27,6 @@
 // a = [-1, 150, 190, 170, -1, -1, 160, 180]
 // sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
 
-// function sortByHeight(val){
-//   const tree = []
-//   const height = []
-//
-//   val.forEach((x, i) => {
-//     if(x === -1){
-//       tree.push(i)
-//     }else{
-//       height.push(x)
-//     }
-//   })
-//   const sort = height.sort((a, b) => a - b)
-//   tree.forEach((x, i) => sort.splice(tree[i], 0, -1))
-//   return sort
-// }
-//
 // console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]))
 
 // CHALLENGE 5: MISSING LETTERS
@@ -88,32 +36,14 @@
 // missingLetters("abcdefghjklmno") == "i"
 // missingLetters("abcdefghijklmnopqrstuvwxyz") == undefined
 
-// function missingLetters(str){
-//   let compare = str.charCodeAt(0)
-//   let missing;
-//
-//   str.split('').map((x, i) => {
-//     if(str.charCodeAt(i) == compare){
-//       ++compare
-//     }else{
-//       missing = String.fromCharCode(compare)
-//     }
-//   })
-//   return missing
-// }
-//
+
 // console.log(missingLetters('abcdefghjklmno'))
 
 // CHALLENGE 6: EVEN & ODD SUMS
 // Take in an array and return an array of the sums of even and odd numbers
 // ex.
 // evenOddSums([50, 60, 60, 45, 71]) == [170, 116]
-//
-// function evenOddSums(arr){
-//   let odd = 0
-//   let even = 0
-//   arr.forEach(x => x % 2 === 0 ? (even+=x) : (odd+=x))
-//   return [even, odd]
-// }
-//
+
+
+
 // console.log(evenOddSums([50, 60, 60, 45, 71]))
--- ChallengesTwo/challenge2.js
+++ ChallengesTwo/challenge2.js
@@ -6,7 +6,6 @@
 
 // Solution 1
 
-//
 // console.log(longestWord('Hello there, my name is Max'))
 
 // CHALLENGE 2: ARRAY CHUNKING
@@ -14,10 +13,6 @@
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
 
-// Solution 1
-
-
-// Solution 2
 
 // console.log(chunkArray([1, 2, 3, 4, 5, 6, 7], 2))
 
@@ -25,26 +20,7 @@
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
 
-// Solution 1
-// function flattenArray(arrays){
-//   return arrays.flat()
-// }
 
-// Solution 2
-// function flattenArray(arrays){
-//   return arrays.reduce((a, b) => a.concat(b))
-// }
-
-// Solution 3
-// function flattenArray(arrays){
-//   return [].concat.apply([], arrays)
-// }
-
-// Solution 4
-// function flattenArray(arrays){
-//   return [].concat(...arrays)
-// }
-//
 // console.log(flattenArray([[1, 2], [3, 4], [5, 6], [7]]))
 
 // CHALLENGE 4: ANAGRAM
@@ -52,13 +28,7 @@
 // ex. 'elbow' === 'below'
 // ex. 'Dormitory' === 'dirty room##'
 
-// Solution 1
-// function isAnagram(str1, str2){
-//   let arr1 = str1.toLowerCase().split('').sort().join('')
-//   let arr2 = str2.toLowerCase().split('').sort().join('')
-//   return arr1 === arr2
-// }
-//
+
 // console.log(isAnagram('dda','DAD'))
 
 // CHALLENGE 5: LETTER CHANGES
@@ -66,18 +36,5 @@
 // Z should turn to A
 // ex. 'hello there' === 'Ifmmp UIfsf'
 
-// function letterChanges(str){
-//   let newStr = str.toLowerCase().replace(/[a-z]/gi, char => {
-//     if(char === 'z' || char === 'Z'){
-//       return 'a'
-//     }else{
-//       return String.fromCharCode(char.charCodeAt() + 1)
-//     }
-//   })
-//   newStr = newStr.replace(/a|e|i|o|u/gi, vowel => {
-//     return vowel.toUpperCase()
-//   })
-//   return newStr
-// }
-//
+
 // console.log(letterChanges('hello there'))
