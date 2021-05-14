11cd3ba80c219a7d65a28790192ee0c1dc65e5e8
Guillermo Aguilar <gaguilar128@gmail.com>
2021-05-13 19:52:00 -0500

    Algo Practice

--- ChallengesThree/challenge3.js
+++ ChallengesThree/challenge3.js
@@ -2,6 +2,18 @@
 // Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
 // ex. addAll(2,5,6,7) === 20
 
+// Solution 1
+// const addAll = (...nums) => nums.reduce((a, b) => a + b, 0)
+
+// Solution 2
+// function addAll(){
+//   let arr = Array.from(arguments)
+//   let sum = 0
+//   for(let i = 0; i < arr.length; i++){
+//     sum+=arr[i]
+//   }
+//   return sum
+// }
 
 // console.log(addAll(2, 5, 6, 7))
 
@@ -10,16 +22,45 @@
 // Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
 // ex. sumAllPrimes(10) == 17
 
+// Solution 1
+// function sumAllPrimes(num){
+//   let i = 2
+//   let total = 0
+//   while(i <= num){
+//     if(num % i === 0){
+//       total+=i
+//     }
+//     i++
+//   }
+//   return total
+// }
 
-// console.log(sumAllPrimes(100))
+// Solution 2
+// function sumAllPrimes(num){
+//   let total = 0
+//   for(let i = 2; i <= num; i++){
+//     if(num % i === 0){
+//       total+=i
+//     }
+//   }
+//   return total
+// }
+
+// console.log(sumAllPrimes(10))
 
 // CHALLENGE 3: SEEK & DESTROY
 // Remove from the array whatever is in the following arguments. Return the leftover values in an array
 // ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
 
+// Solution 1
+const seekAndDestroy = (arr, val1, val2) => arr.filter(x => x !== val1 && x !== val2)
 
-// console.log(seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6))
+// Solution 2
 
+
+
+
+console.log(seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6))
 
 // CHALLENGE 4: SORT BY HEIGHT
 // Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
